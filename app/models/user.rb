8e7a62dcf382ace97351c783bca23732c8292d41
fy-0621 <60965937+fy-0621@users.noreply.github.com>
2020-03-14 14:39:11 +0900

    chat-space

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+<<<<<<< Updated upstream
+=======
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+>>>>>>> Stashed changes
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
